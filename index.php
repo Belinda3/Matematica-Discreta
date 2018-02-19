23c46e46a93d704660577458aebca1182d110ea3
Unknown <hrodrigue26@uniminuto.edu.co>
2018-02-18 20:16:51 -0500

    creacion con esqueleto de proyecto librerias y primer diseño

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bpp.css">
+    <!-- Semantic UI-->
+    <link rel="stylesheet" href="css/semantic/dist/semantic.min.css">
+    <!-- fuentes -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+  </head>
+  <title>Matematica Discreta</title>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <br>
+  <h2 class="text-white text-center">Matematica discreta</h2>
+  <br>
+</nav>
+
+<div class="jumbotron">
+<div class="ui grid">
+  <div class="three wide column"></div>
+  <div class="three wide column">
+    <div class="ui card">
+  <div class="image">
+    <img src="" alt="">
+  </div>
+  <div class="content">
+    <a href="primero.php" class="header">Primer corte</a>
+    <div class="description">Proyecto de primer corte para la materia Matematica Discreta</div>
+  </div>
+  </div></div>
+  <div class="three wide column"><div class="ui card">
+  <div class="image">
+    <img src="" alt="">
+  </div>
+  <div class="content">
+    <a href="segundo.php" class="header">Segundo corte</a>
+    <div class="description">Proyecto de segundo corte para la materia Matematica Discreta</div>
+  </div>
+  </div></div>
+  <div class="three wide column"><div class="ui card">
+  <div class="image">
+    <img src="" alt="">
+  </div>
+  <div class="content">
+    <a href="tercero.php" class="header">Tercer corte</a>
+    <div class="description">Proyecto de tercer corte para la materia Matematica Discreta</div>
+  </div>
+  </div></div>
+  <div class="three wide column"></div>
+</div>
+</div>
+
+
+<!--
+<div class="ui card">
+<div class="image">
+  <img src="" alt="">
+</div>
+<div class="content">
+  <a href="primero.php" class="header">Primer corte</a>
+  <div class="description">Proyecto de primer corte para la materia Matematica Discreta</div>
+</div>
+</div>
+-->
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery.min.js"></script>
+    <script src="css/semantic/dist/semantic.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
