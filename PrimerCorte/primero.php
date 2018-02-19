7e9a914063eff84d182580a13e596abd03bf1ce3
Unknown <hrodrigue26@uniminuto.edu.co>
2018-02-19 10:56:53 -0500

    Primer corte maqueta de insercion de datos

---
+++ PrimerCorte/primero.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../css/bpp.css">
+  <!-- Semantic UI-->
+  <link rel="stylesheet" href="../css/semantic/dist/semantic.min.css">
+  <!-- fuentes -->
+  <link rel="stylesheet" href="../css/font-awesome.min.css">
+  <title>Primer corte</title>
+</head>
+<body>
+  <nav class="navbar navbar-dark bg-dark">
+    <br>
+<h2 class="text-white text-center">Matematica discreta</h2>
+<br>
+</nav>
+</body>
+</html>
