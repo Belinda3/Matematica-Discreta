8dc01af23ea8e5858c9806718bb542a33f6ac58d
Unknown <hrodrigue26@uniminuto.edu.co>
2018-02-24 23:39:49 -0500

    actualizacion de maqueta

--- PrimerCorte/primero.php
+++ PrimerCorte/primero.php
@@ -7,16 +7,48 @@
   <link rel="stylesheet" href="../css/bootstrap.css">
   <link rel="stylesheet" href="../css/bpp.css">
   <!-- Semantic UI-->
-  <link rel="stylesheet" href="../css/semantic/dist/semantic.min.css">
+
   <!-- fuentes -->
   <link rel="stylesheet" href="../css/font-awesome.min.css">
   <title>Primer corte</title>
 </head>
 <body>
-  <nav class="navbar navbar-dark bg-dark">
-    <br>
-<h2 class="text-white text-center">Matematica discreta</h2>
-<br>
+<nav class="navbar navbar-dark bg-dark">
+<h3 class="text-white text-center">Matematica discreta</h3>
 </nav>
+<div class="jumbotron">
+  <h2 class="text-center">Casos de Factorizacion</h2>
+  <br>
+  <br>
+  <div class="row">
+    <div class="col-lg-1">&nbsp;</div>
+    <div class="col-lg-3"><h5 class="text-center">Trinomio cuadrado perfecto</h5>
+
+    </div>
+    <div class="col-lg-4">
+    <h5 class="text-center">Trinomio de la forma x²+bx+c</h5>
+      <div class="container">
+        <div class="row">
+          <form action="Trinomio2.php" method="post">
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+          x²+<input type="number" size="10">x+ <input type="number" size="10">
+          </form>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-lg-3"><h5 class="text-center">Trinomio de la forma ax²+bx+c</h5>
+
+    </div>
+    <div class="col-lg-1">&nbsp;</div>
+  </div>
+</div>
 </body>
 </html>
